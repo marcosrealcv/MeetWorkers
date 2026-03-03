490307fb0a341696cc9278a9bb68d630cbfd21da
marcosrealcv <marcos.realp@gmail.com>
2026-03-03 18:46:14 +0100

    implemento el control de rutas mediante router y tambien agrego estrucutra basica del login de usuario

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './componentes/zonaCliente/login/login';
 
-export const routes: Routes = [];
+export const rutasApp: Routes = [ 
+
+  {
+    path: 'login',
+    component: Login
+  }
+
+]
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/zonaTienda/header/header';
 import { Footer } from './componentes/zonaTienda/footer/footer';
+import { filter } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,17 @@ import { Footer } from './componentes/zonaTienda/footer/footer';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  
+  protected readonly mostrarHeaderFooter = signal(true);
+
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.mostrarHeaderFooter.set(event.urlAfterRedirects !== '/login');
+    });
+  }
+
 }
